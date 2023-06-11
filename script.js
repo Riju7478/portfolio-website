b121fc3bb99d89f9d14269e64e209fd3105e859f
Riju Rakshit <riju@gamil.com>
2023-06-11 09:25:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("script running.....")
+document.querySelector('.cross').getElementsByClassName.display = "none";
+document.querySelector('.cross').addEventListener.toggle('sidebarGo')
+if(document.querySelector('.sidebar').classList.contains('sidebarGo')
